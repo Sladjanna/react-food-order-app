4427ac5b2b2a12985c722edc84f6a93dc29e0bb1
Sladjana Kalanj <k.sladjana@gmail.com>
2023-01-13 11:51:33 +0100

    add modal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <>
+      <Backdrop>
+        <ModalOverlay>{props.children}</ModalOverlay>
+      </Backdrop>
+    </>
+  );
+};
+
+export default Modal;
